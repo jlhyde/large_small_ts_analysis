3b26b2ac2d9936453f3ed53f8c991705a70dbe4c
Jacy Hyde <jlhyde@ufl.edu>
2016-11-18 12:08:47 -0500

    Change file name
    houseelf-earlength-dna-data_1

--- houseelf_analysis.R
+++ houseelf_analysis.R
@@ -1,5 +1,5 @@
 ##analysis of houseelf earsizes with Dr. Granger
 
 #import data
-data <- read.csv("./data/houseelf-earlength-dna-data.csv")
+data <- read.csv("./data/houseelf-earlength-dna-data_1.csv")
 
